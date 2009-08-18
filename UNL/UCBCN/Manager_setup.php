48c9b26fc3bb6247bafe39f7e2b2363052d5cc3e
Brett Bieber <brett.bieber@gmail.com>
2009-08-18 15:50:55 +0000

    Add a newline and <br /> to the messages when no UI is set so it's a little more readable if you execute the setup scripts directly.

--- UNL/UCBCN/Manager_setup.php
+++ UNL/UCBCN/Manager_setup.php
@@ -275,7 +275,7 @@ class UNL_UCBCN_Manager_setup_postinstall
         if (isset($this->_ui)) {
             $this->_ui->outputData($msg);
         } else {
-            echo $msg;
+            echo $msg.'<br />'.PHP_EOL;
         }
     }
 }
